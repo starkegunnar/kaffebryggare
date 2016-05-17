0d9b9a93e730d23405677b644285cfc89671ede1
martin <martin.george89@gmail.com>
2016-05-17 21:29:50 +0200

    improved generality

--- raspi-python/testtest.py
+++ raspi-python/testtest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #encoding: utf-8
+import os
 import sys
 import random
 import bluetooth
@@ -9,6 +10,8 @@ cups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 nohandle = [",", "!"]
 nohash = [".", "!", " "]
 
+os.path.expanduser('~user')
+
 def getHandle(stringArray, defaultArray, oneIn):
 	if random.randint(1, oneIn) == oneIn:
 		return " " + random.choice(stringArray) + random.choice(defaultArray)
@@ -16,7 +19,7 @@ def getHandle(stringArray, defaultArray, oneIn):
 		return random.choice(defaultArray)
 
 #Read Config file
-file = open('/home/pi/twitter/kaffe-conf.txt','r')
+file = open(os.path.expanduser('~') + '/twitter-conf.txt','r')
 conf = file.read().splitlines()
 file.close()
 
@@ -32,25 +35,25 @@ access_token_secret = conf[4]
 api = Twython(api_key, api_secret, access_token, access_token_secret)
 
 #Twitter strings
-file = open('/home/pi/twitter/kaffe-greetings.txt','r')
+file = open('kaffe-greetings.txt','r')
 greets = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-verbs.txt','r')
+file = open('kaffe-verbs.txt','r')
 verbs = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-names.txt','r')
+file = open('kaffe-names.txt','r')
 names = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-containers.txt', 'r')
+file = open('kaffe-containers.txt', 'r')
 conts = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-verbs2.txt', 'r')
+file = open('kaffe-verbs2.txt', 'r')
 verbs2 = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-hashtags.txt', 'r')
+file = open('kaffe-hashtags.txt', 'r')
 hashtags = file.read().splitlines()
 file.close()
-file = open('/home/pi/twitter/kaffe-handles.txt', 'r')
+file = open('kaffe-handles.txt', 'r')
 handles = file.read().splitlines()
 file.close()
 
