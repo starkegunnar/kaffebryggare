4b71b2d87c4590dd727763af5468e7fdab87d3ed
martin <martin.george89@gmail.com>
2016-05-31 12:24:43 +0200

    Changed phrases into English

--- raspi-python/testtest.py
+++ raspi-python/testtest.py
@@ -19,17 +19,6 @@ startFiles = ['kaffe-greetings.txt', 'kaffe-verbs.txt', 'kaffe-names.txt']
 doneFiles = ['kaffe-verbs2.txt', 'kaffe-containers.txt', 'kaffe-names.txt']
 # Date-time initialization.
 
-class timestamp(object):
-	def __init__(self):
-		self.lock = threading.Lock()
-		self.value = datetime.now()
-	def refresh(self):
-		self.lock.acquire()
-		try:
-			self.value = datetime.now()
-		finally:
-			self.lock.release()
-
 def getHandle(chance):
 	if random.randint(1, chance) == chance:
 		return " " + getPhrase('kaffe-handles.txt') + random.choice(nohandle)
@@ -55,53 +44,6 @@ def composeMessage(messageType):
 		return getPhrase(doneFiles[0]) + " " + random.choice(cups) + " " + getPhrase(doneFiles[1]) + " " + getPhrase(doneFiles[2]) + getHashtag(3)
 	else:	
 		return "Ooops"
-
-def messagePoll(t):
-	try:
-		while 1:
-			raw_input()
-			t.refresh()
-			print "Timestamp refreshed on keypress"
-			print t.value
-	except KeyboardInterrupt:
-		print "Exiting messagePoll"
-		return
-
-def timePoll(t):
-	try:
-		while 1:
-			if datetime.now().minute > t.value.minute:
-				t.refresh()
-				print "timestamp refreshed on timer"
-				print t.value
-			time.sleep(1)
-	except KeyboardInterrupt:
-		print "Exiting timePoll"
-		return
-
-ts = timestamp()
-print ts.value
-print ts.value.minute
-t1 = threading.Thread(target=messagePoll, args=(ts,))
-t2 = threading.Thread(target=timePoll, args=(ts,))
-t1.start()
-t2.start()
-
-#Read Config file
-file = open(os.path.expanduser('~') + '/twitter-conf.txt','r')
-conf = file.read().splitlines()
-file.close()
-
-#Bluetooth constants
-bluetoothAddr = conf[0]
-port = 1
-
-#Twitter constants
-api_key = conf[1]
-api_secret = conf[2]
-access_token = conf[3]
-access_token_secret = conf[4]
-api = Twython(api_key, api_secret, access_token, access_token_secret)
 
 coffeeStart = "TEST: " + composeMessage('start')
 coffeeDone = "TEST: " + composeMessage('done')
