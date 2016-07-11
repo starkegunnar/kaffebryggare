7d8b116279196e7677755ece343b314154cb3dac
Martin George <martin.george89@gmail.com>
2016-07-11 13:15:32 +0200

    added choices to install

--- install.sh
+++ install.sh
@@ -26,39 +26,43 @@ else
 fi
 
 while true; do
-    read -p "Do you wish to install a cronjob for kaffeTweet?" yn
+    read -p "Do you wish to install a cronjob for kaffeTweet? " yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "*** Finished!"; exit;;
+        [Yy]* ) # Install crontab job
+			echo "*** Installing crontab jobs to /etc/cron.d/kaffecron";
+			echo "@reboot root python $SCRIPTPATH/raspi-python/kaffeTweet.py  >> $SCRIPTPATH/logs/kaffe.log 2>&1" >  /etc/cron.d/kaffecron;
+			echo "0 3 * * 1 root python $SCRIPTPATH/raspi-python/statTweet.py >> $SCRIPTPATH/logs/kaffe.log 2>&1" >> /etc/cron.d/kaffecron;
+			echo "*** Cronjob installed successfully."
+			break;;
+        [Nn]* ) 
+			echo "*** Will not install a cronjob."; 
+			break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-# Install crontab job
-echo "*** Installing crontab jobs to /etc/cron.d/kaffecron"
-echo "@reboot root python $SCRIPTPATH/raspi-python/kaffeTweet.py  >> $SCRIPTPATH/logs/kaffe.log 2>&1" >  /etc/cron.d/kaffecron
-echo "0 3 * * 1 root python $SCRIPTPATH/raspi-python/statTweet.py >> $SCRIPTPATH/logs/kaffe.log 2>&1" >> /etc/cron.d/kaffecron
-
 while true; do
-    read -p "Do you wish to install logrotate for kaffeTweet? " yn
+    read -p "Do you wish to configure logrotate for kaffeTweet? " yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "*** Finished!"; exit;;
+        [Yy]* ) # Install logrotate 
+			echo "*** Installing logrotate configuration to /etc/logrotate.d/kaffelog";
+			echo "$SCRIPTPATH/logs/kaffe.log {" 	>  /etc/logrotate.d/kaffelog;
+			echo "	rotate 7" 						>> /etc/logrotate.d/kaffelog;
+			echo "	daily" 							>> /etc/logrotate.d/kaffelog;
+			echo "	compress" 						>> /etc/logrotate.d/kaffelog;
+			echo "	copytruncate" 					>> /etc/logrotate.d/kaffelog;
+			echo "	notifempty" 					>> /etc/logrotate.d/kaffelog;
+			echo "}" 								>> /etc/logrotate.d/kaffelog;
+			echo "*** Configuration for logrotate installed successfully.";
+			break;;
+        [Nn]* ) 
+			echo "*** Will not configure logrotate.";
+			break;;
         * ) echo "Please answer yes or no. ";;
     esac
 done
 
-# Install logrotate 
-echo "*** Installing logrotate configuration to /etc/logrotate.d/kaffelog"
-echo "$SCRIPTPATH/logs/kaffe.log {" 	>  /etc/logrotate.d/kaffelog
-echo "	rotate 7" 						>> /etc/logrotate.d/kaffelog
-echo "	daily" 							>> /etc/logrotate.d/kaffelog
-echo "	compress" 						>> /etc/logrotate.d/kaffelog
-echo "	copytruncate" 					>> /etc/logrotate.d/kaffelog
-echo "	notifempty" 					>> /etc/logrotate.d/kaffelog
-echo "}" 								>> /etc/logrotate.d/kaffelog
-
-echo "*** Finished!"
+echo "*** Done!"
 # Clean-up for testing.
 #rm /etc/cron.d/kaffecron
 #rm /etc/logrotate.d/kaffelog
